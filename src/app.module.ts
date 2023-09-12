f0f1ac46c4805e85639de54cda798e1f9ab99ce6
arifiensr-gobimbel <arifiensr.gobimbel@gmail.com>
2023-09-12 13:25:51 +0700

    Connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { AuthController } from './auth/auth.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AuthController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
